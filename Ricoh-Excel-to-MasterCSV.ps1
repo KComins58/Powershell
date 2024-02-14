c1e84c56429c9272273ab817b135a23fded97d14
Kyle Comins <136008404+KComins58@users.noreply.github.com>
2024-02-14 15:41:16 -0600

    Update Ricoh-Excel-to-MasterCSV.ps1

--- Ricoh-Excel-to-MasterCSV.ps1
+++ Ricoh-Excel-to-MasterCSV.ps1
@@ -37,8 +37,8 @@ function Process-Row {
     $sEmail = $sUsername.ToLower()+"@email.com"
 
     if ($Column4 -eq 'teacher') {
-        $sPath = "\\fscluster1\teacher$\"+$sUsername.ToLower()+"\Documents\scan"
-    } else { $sPath = "\\fscluster1\office$\"+$sUsername.ToLower()+"\Documents\scan" }
+        $sPath = "\\Domain\teacher$\"+$sUsername.ToLower()+"\Documents\scan"
+    } else { $sPath = "\\Domain\office$\"+$sUsername.ToLower()+"\Documents\scan" }
 
     switch -Regex ($Column2[0]) {
     "^[A-B]" {$K = "1"}
@@ -74,11 +74,9 @@ $header = @"
 Index in ACLs and Groups,Name,Set General Settings,Set Registration No.,Registration No.,Entry Type,Phonetic Name,Display Name,Display Priority,Set Title Settings,Title 1,Title 2,Title 3,Title Freq.,Set User Code Settings,User Code,Set Auth. Info Settings,Device Login User Name,Device Login Password,Device Login Password Encoding,SMTP Authentication,SMTP Authentication Login User Name,SMTP Authentication Login Password,SMTP Authentication Password Encoding,Folder Authentication,Folder Authentication Login User Name,Folder Authentication Login Password,Folder Authentication Password Encoding,LDAP Authentication,LDAP Authentication Login User Name,LDAP Authentication Login Password,LDAP Authentication Password Encoding,Set Access Control Settings,Can Use B/W Copy,Can Use Single Color Copy,Can Use Two Color Copy,Can Use Full Color Copy,Can Use Auto Color Copy,Can Use B/W Print,Can Use Color Print,Can Use Scanner,Can Use Fax,Can Use Document Server,Maximum of Print Usage Limit,Set Email/Fax Settings,Fax Destination,Fax Line Type,International Fax Transmission Mode,E-mail Address,Ifax Address,Ifax Enable,Direct SMTP,Ifax Direct SMTP,Fax Header,Label Insertion 1st Line (Selection),Label Insertion 2nd Line (String),Label Insertion 3rd Line (Standard Message),Set Folder Settings,Folder Protocol,Folder Port No.,Folder Server Name,Folder Path,Folder Japanese Character Encoding,Set Protection Settings,Is Setting Destination Protection,Is Protecting Destination Folder,Is Setting Sender Protection,Is Protecting Sender,Sender Protection Password,Sender Protection Password Encoding,Access Privilege to User,Access Privilege to Protected File,Set Group List Settings,Groups,Set Counter Reset Settings,Enable Plot Counter Reset,Enable Fax Counter Reset,Enable Scanner Counter Reset,Enable User Volume Counter Reset
 "@
 
-$header | Out-File -FilePath $csvFileName
-
-$importedData = Import-Excel -Path $importfile -WorksheetName "Sheet2"
-
 $iCount = 1
+
+$header | Out-File -FilePath $csvFileName -Encoding UTF8
 
 $importedData | ConvertTo-Csv -NoTypeInformation | Select-Object -Skip 1 | ForEach-Object {
     $row = $_
@@ -87,10 +85,13 @@ $importedData | ConvertTo-Csv -NoTypeInformation | Select-Object -Skip 1 | ForEa
     # Check if the first column is empty
     if (-not [string]::IsNullOrWhiteSpace($rowValues[0])) {
         $processedRow = Process-Row -iCount $iCount -Column1 $rowValues[0] -Column2 $rowValues[1] -Column3 $rowValues[2] -Column4 $rowValues[3]
-        $processedRow -join ',' | Out-File -FilePath $csvFileName -Append
+        $processedRow -join ',' | Out-File -FilePath $csvFileName -Append -Encoding UTF8
         $iCount += 1
     } else {
         # Exit the loop if the first column is empty
         break
     }
 }
+
+# Remove the BOM from the file
+(Get-Content -Raw -Path $csvFileName) | Set-Content -Path $csvFileName -Encoding UTF8
